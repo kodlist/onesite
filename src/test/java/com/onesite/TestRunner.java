cbd6027e866202c062b71ad1c58e24c6150989f2
meharlist <kmehar650@gmail.com>
2019-01-26 22:54:23 +0530

    currenlty project can run in jenkins but no html report will be seen, the other party has to setup jenkins in his or her machine to see jenkins cucumber reports

--- src/test/java/com/onesite/TestRunner.java
+++ src/test/java/com/onesite/TestRunner.java
@@ -17,18 +17,18 @@ import com.onesite.managers.FileReaderManager;
 import com.onesite.stepdefinitions.CreateComputerPageSteps;
 
 //plugin = { "com.cucumber.listener.ExtentCucumberFormatter:target/cucumber-reports/report.html"}
-
+//plugin = {"pretty", "html:target/cucumber", "json:target/cucumber.json"},
 
   @RunWith(Cucumber.class)
   @CucumberOptions( features = {"src/test/resources"},		  			
-		  			plugin = {"pretty", "html:target/cucumber", "json:target/cucumber.json"},					
+		            plugin = { "com.cucumber.listener.ExtentCucumberFormatter:target/cucumber-reports/report.html"},
 		  		    monochrome = true,
   					glue={"com.onesite.stepdefinitions", "com.onesite.hooks"},
   					tags={"@createComputerScenario,@readComputerScenario, @updateComputerScenario, @deleteComputerScenario" } )
   public class TestRunner {
 	  private static final Logger LOGGER = LogManager.getLogger(TestRunner.class);
 	  
-	  /*@AfterClass
+	  @AfterClass
 	  public static void writeExtentReport() {
 		  Reporter.loadXMLConfig(new File(FileReaderManager.getInstance().getConfigReaderInstance().getReportConfigPath()));
 		  Reporter.setSystemInfo("User Name", System.getProperty("user.name"));
@@ -39,6 +39,6 @@ import com.onesite.stepdefinitions.CreateComputerPageSteps;
 		  
 		  LOGGER.info("----- Generating html report with test results, and you should see them in cucumber-reports under target directory.... ------ ");
 		  
-	  }*/
+	  }
 
   }
